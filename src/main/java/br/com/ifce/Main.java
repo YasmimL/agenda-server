04adc19992d802f8a546d2d64d208e9167ddb500
YasmimL <yasmimlima1601@gmail.com>
2024-09-23 16:29:04 -0300

    help message

--- src/main/java/br/com/ifce/Main.java
+++ src/main/java/br/com/ifce/Main.java
@@ -8,9 +8,15 @@ import br.com.ifce.repository.AgendaRepository;
 public class Main {
 
     public static void main(String[] args) {
-        if (args.length < 1) return;
+        if (args.length < 1) {
+            System.out.println("Please provide a valid agenda (Ex.: agenda1, agenda2, agenda3)");
+            return;
+        }
         final var agenda = Agenda.from(args[0]);
-        if (agenda == null) return;
+        if (agenda == null) {
+            System.out.println("Please provide a valid agenda (Ex.: agenda1, agenda2, agenda3)");
+            return;
+        }
 
         AgendaRepository.newInstance(agenda);
         SyncService.syncSelf();
